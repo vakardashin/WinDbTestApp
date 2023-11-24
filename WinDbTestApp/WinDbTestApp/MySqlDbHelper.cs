38caaa2cb1fdeec663f906e9291e59c0ba26937c
VladimirKardashin <kardv@yandex.ru>
2023-11-24 19:58:38 +0300

    Установка связи с MySql сервером

--- WinDbTestApp/WinDbTestApp/MySqlDbHelper.cs
+++ WinDbTestApp/WinDbTestApp/MySqlDbHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,19 @@ namespace WinDbTestApp
     {
       public static string DoDbTest()
         {
-            string s = "Hello?";
+            string s = " ";
+            string strCnn = "Server=localhost;User ID = root;Password=root;Database=arnion";
+            using(var cnn = new MySqlConnection(strCnn))
+            {
+                var cmd = cnn.CreateCommand();
+                cmd.CommandText = "SELECT 1";
+                cnn.Open();
+                var rdr = cmd.ExecuteReader();
+                while(rdr.Read())
+                {
+                    s += (string)rdr[0].ToString() + " ";
+                }
+            }
             return s;
         }
     }
