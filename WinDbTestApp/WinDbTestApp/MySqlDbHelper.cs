02fead33ea8e984bead268ff214dd6cf732167d7
VladimirKardashin <kardv@yandex.ru>
2023-11-26 18:29:16 +0300

    Некоторые изменения в запросе к MySql

--- WinDbTestApp/WinDbTestApp/MySqlDbHelper.cs
+++ WinDbTestApp/WinDbTestApp/MySqlDbHelper.cs
@@ -12,16 +12,21 @@ namespace WinDbTestApp
         public static string DoDbTest()
         {
             string s = " ";
+            // строка подключения к БД
             string strCnn = "Server=localhost;User ID = root;Password=root;Database=arnion";
-            using(var cnn = new MySqlConnection(strCnn))
+            // создаём объект для подключения к БД
+            using (var cnn = new MySqlConnection(strCnn))
             {
                 var cmd = cnn.CreateCommand();
-                cmd.CommandText = "SELECT 1";
+                // запрос
+                cmd.CommandText = "SELECT id_categories, categories_name FROM goods_categories WHERE id_categories >= 11";
+                // устанавливаем соединение с БД
                 cnn.Open();
                 var rdr = cmd.ExecuteReader();
-                while(rdr.Read())
+                // читаем результат
+                while (rdr.Read())
                 {
-                    s += (string)rdr[0].ToString() + " ";
+                    s += (string)rdr[0].ToString() + " " + rdr[1].ToString();
                 }
             }
             return s;
